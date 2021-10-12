d340f4535582551fd9f8b12d38af2ad849210eae
Chane-Hall <54103462+Chane-Hall@users.noreply.github.com>
2021-10-12 11:08:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const home = document.getElementById('home');
+const menu = document.getElementById('menu');
+const about = document.getElementById('about');
+const merch = document.getElementById('merch');
+const project1 = document.getElementById('project1');
+const project2 = document.getElementById('project2');
+const project3 = document.getElementById('project3');
