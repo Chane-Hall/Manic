bce457e912947564ed6df961bb16c45d54a17ac4
Christoff-png <70331006+Christoff-png@users.noreply.github.com>
2021-11-19 12:01:59 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	
-	console.log("Page Loaded, loading plugin");
-	const swup = new Swup({
-  		plugins: [new SwupPreloadPlugin()]
-	});
-	console.log("Preloading pages");
-	swup.preloadPages();	
-}
